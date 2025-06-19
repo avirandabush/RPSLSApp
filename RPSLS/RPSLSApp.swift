9b4f260a3a4c53eaa78b2de928b417f548701f52
aviran <avirand@balink.net>
2025-06-19 10:51:42 +0300

    init project.

---
+++ RPSLS/RPSLSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSLSApp.swift
+//  RPSLS
+//
+//  Created by Aviran Dabush on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct RPSLSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
